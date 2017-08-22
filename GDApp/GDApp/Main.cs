fbb7ed639ce5e6b4fe9f3dd3d8e59f73f678e13c
Niall McGuinness <niall.mcguinness@dkit.ie>
2017-08-22 10:53:31 +0100

    Tidied Main::Initialize() in preparation for adding the ground and skybox planes.

--- GDApp/GDApp/Main.cs
+++ GDApp/GDApp/Main.cs
@@ -1,3 +1,4 @@
+using System;
 using GDLibrary;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -16,6 +17,7 @@ namespace GDApp
         private CameraManager cameraManager;
         private MouseManager mouseManager;
         private KeyboardManager keyboardManager;
+        private BasicEffect modelEffect;
 
         public ObjectManager objectManager { get; private set; }
 
@@ -35,58 +37,32 @@ namespace GDApp
         {
             #region Set the screen resolution
             //obviously this will affect the viewport for the camera and does use the same aspect ratio as the camera i.e. 4/3
-            int width = 1024, height = 768; //4.3 ratiow
-            graphics.PreferredBackBufferWidth = width;
-            graphics.PreferredBackBufferHeight = height;
-            //if we forget to apply the changes then our resolution wont be set!
-            graphics.ApplyChanges();
+            int resolutionWidth = 1024, resolutionHeight = 768;
+            InitializeGraphics(resolutionWidth, resolutionHeight);
             #endregion
 
             #region Initialize the effect (shader) for models
-            BasicEffect modelEffect = new BasicEffect(graphics.GraphicsDevice);
-            //enable the use of a texture on a model
-            modelEffect.TextureEnabled = true;
-            //setup the effect to have a single default light source which will be used to calculate N.L and N.H lighting
-            modelEffect.EnableDefaultLighting();
+            InitializeEffects();
             #endregion
 
-            #region Add the CameraManager and ObjectManager
-            this.cameraManager = new CameraManager(this, 1);
-            Components.Add(this.cameraManager);
-
-            this.objectManager = new ObjectManager(this, cameraManager, 10);
-            Components.Add(this.objectManager);
+            #region Add the Managers
+            bool isMouseVisible = true;
+            InitializeManagers(resolutionWidth, resolutionHeight, isMouseVisible);
             #endregion
 
-            #region Add the input managers
-            //add mouse and keyboard managers used by any actor with a controller based on a UserInputController
-            this.mouseManager = new MouseManager(this, true, new Vector2(width/2.0f, height/2.0f));
-            Components.Add(this.mouseManager);
-            this.keyboardManager = new KeyboardManager(this);
-            Components.Add(this.keyboardManager);
+            #region Add Camera(s)
+            InitializeCameras(resolutionWidth, resolutionHeight);
             #endregion
 
-            //we will use this variable for the camera and re-use for the modelobject
-            Transform3D transform = null;
-
-            #region Add the camera 
-            //add the camera
-            transform = new Transform3D(new Vector3(0, 0, 10), -Vector3.UnitZ, Vector3.UnitY);
-
-            //set the camera to occupy the entire viewport
-            Viewport viewPort = new Viewport(0, 0, width, height);
-            //initialise the camera
-            Camera3D firstPersonCamera = new Camera3D("first person camera 1", ActorType.Camera, transform, ProjectionParameters.StandardMediumFourThree, viewPort, 0, StatusType.Drawn | StatusType.Update);
-            //attach a FirstPersonCameraController
-            firstPersonCamera.AttachController(new FirstPersonController("firstPersonCameraController1", ControllerType.FirstPerson,
-                AppData.CameraMoveKeys, AppData.CameraMoveSpeed, AppData.CameraStrafeSpeed, AppData.CameraRotationSpeed,
-                this.mouseManager, this.keyboardManager, this.cameraManager));
-
-
-            this.cameraManager.Add(firstPersonCamera);
+            #region Add ModelObject(s)
+            AddDecoratorModelObjects();
             #endregion
 
-            #region Add the model object
+            base.Initialize();
+        }
+
+        private void AddDecoratorModelObjects()
+        {
             //load the texture
             Texture2D texture = Content.Load<Texture2D>("Assets/Textures/Props/Crates/crate1");
 
@@ -94,9 +70,9 @@ namespace GDApp
             Model boxModel = Content.Load<Model>("Assets/Models/box2");
 
             //use one of our static defaults to position the object at the origin
-            transform = Transform3D.Zero;
+            Transform3D transform = Transform3D.Zero;
             //initialise the boxObject
-            ModelObject boxObject = new ModelObject("box1", ActorType.Decorator, transform, modelEffect, Color.White, 1, texture, boxModel);
+            ModelObject boxObject = new ModelObject("box1", ActorType.Decorator, transform, this.modelEffect, Color.White, 1, texture, boxModel);
             //add to the objectManager so that it will be drawn and updated
             this.objectManager.Add(boxObject);
 
@@ -115,9 +91,60 @@ namespace GDApp
             //add more clones here...
 
 
-            #endregion
+        }
 
-            base.Initialize();
+        private void InitializeCameras(int resolutionWidth, int resolutionHeight)
+        {
+            Transform3D transform = null;
+
+            //add the camera
+            transform = new Transform3D(new Vector3(0, 0, 10), -Vector3.UnitZ, Vector3.UnitY);
+
+            //set the camera to occupy the entire viewport
+            Viewport viewPort = new Viewport(0, 0, resolutionWidth, resolutionHeight);
+            //initialise the camera
+            Camera3D firstPersonCamera = new Camera3D("first person camera 1", ActorType.Camera, transform, ProjectionParameters.StandardMediumFourThree, viewPort, 0, StatusType.Drawn | StatusType.Update);
+            //attach a FirstPersonCameraController
+            firstPersonCamera.AttachController(new FirstPersonController("firstPersonCameraController1", ControllerType.FirstPerson,
+                AppData.CameraMoveKeys, AppData.CameraMoveSpeed, AppData.CameraStrafeSpeed, AppData.CameraRotationSpeed,
+                this.mouseManager, this.keyboardManager, this.cameraManager));
+
+
+            this.cameraManager.Add(firstPersonCamera);
+        }
+
+        private void InitializeManagers(int resolutionWidth, int resolutionHeight, bool isMouseVisible)
+        {
+            this.cameraManager = new CameraManager(this, 1);
+            Components.Add(this.cameraManager);
+
+            this.objectManager = new ObjectManager(this, cameraManager, 10);
+            Components.Add(this.objectManager);
+
+            //add mouse and keyboard managers
+            this.mouseManager = new MouseManager(this, isMouseVisible, /*screen centre*/ new Vector2(resolutionWidth / 2.0f, resolutionHeight / 2.0f));
+            Components.Add(this.mouseManager);
+
+            this.keyboardManager = new KeyboardManager(this);
+            Components.Add(this.keyboardManager);
+
+        }
+
+        private void InitializeEffects()
+        {
+            this.modelEffect = new BasicEffect(graphics.GraphicsDevice);
+            //enable the use of a texture on a model
+            modelEffect.TextureEnabled = true;
+            //setup the effect to have a single default light source which will be used to calculate N.L and N.H lighting
+            modelEffect.EnableDefaultLighting();
+        }
+
+        private void InitializeGraphics(int resolutionWidth, int resolutionHeight)
+        {
+            graphics.PreferredBackBufferWidth = resolutionWidth;
+            graphics.PreferredBackBufferHeight = resolutionHeight;
+            //if we forget to apply the changes then our resolution wont be set!
+            graphics.ApplyChanges();
         }
 
         /// <summary>
