added1265f754a87e2ac586af1b6003af769bd30
Andrew Carolan <andycarolan9@gmail.com>
2019-10-29 15:38:19 +0000

    Aligned the long right wall with the ground plane

--- GDApp/GDApp/App/Main.cs
+++ GDApp/GDApp/App/Main.cs
@@ -496,6 +496,15 @@ namespace GDApp
             this.objectManager.Add(clonePlane);
             #endregion
 
+            #region long right wall
+            clonePlane = (ModelObject)planePrototypeModelObject.Clone();
+            clonePlane.EffectParameters.Texture = this.textureDictionary["right"];
+            clonePlane.Transform.Rotation = new Vector3(90, -90, 0);
+            clonePlane.Transform.Scale = new Vector3(clonePlane.Transform.Scale.X / 1.7f, 1, worldScale / 2);
+            clonePlane.Transform.Translation = new Vector3((2.54f * worldScale) / 13.0f, 0, 90.8f);
+            this.objectManager.Add(clonePlane);
+            #endregion
+
             ////add the top skybox plane
             //clonePlane = (ModelObject)planePrototypeModelObject.Clone();
             //clonePlane.EffectParameters.Texture = this.textureDictionary["sky"];
