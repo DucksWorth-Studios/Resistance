837e04663ed6fa2f1a7c0dafed324a9c9aac6654
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-11-08 20:49:20 +0000

    Add timer utility
    This will be used to hold the initial timer data.

---
+++ GDLibrary/GDLibrary/Utility/TimerUtility.cs
@@ -0,0 +1,57 @@
+﻿namespace GDLibrary
+{
+    public class TimerUtility
+    {
+        #region Fields
+
+        private int hours = 0;
+        private int minutes = 0;
+        private int seconds = 0;
+
+        #endregion
+
+        #region Constructors
+
+        public TimerUtility(int minutes)
+        {
+            this.minutes = minutes;
+        }
+
+        public TimerUtility(int hours, int minutes)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        public TimerUtility(int hours, int minutes, int seconds)
+        {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Hours
+        {
+            get => hours;
+            set => hours = value;
+        }
+
+        public int Minutes
+        {
+            get => minutes;
+            set => minutes = value;
+        }
+
+        public int Seconds
+        {
+            get => seconds;
+            set => seconds = value;
+        }
+
+        #endregion
+    }
+}
