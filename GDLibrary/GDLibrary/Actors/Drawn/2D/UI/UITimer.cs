a625b6ef45e2a53935f77585cbe8a1cc3ac544c3
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-11-10 19:29:11 +0000

    Add UI Timer Class
    This will be used to draw a timer on screen

---
+++ GDLibrary/GDLibrary/Actors/Drawn/2D/UI/UITimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GDLibrary
+{
+    public class UITimer : UITextObject
+    {
+        private TimerUtility timer;
+
+        public UITimer(string id, Transform2D transform, Color color, SpriteEffects spriteEffects, 
+            float layerDepth, SpriteFont spriteFont, TimerUtility timer) : 
+            base(id, ActorType.UIDynamicText, StatusType.Drawn, transform, color, spriteEffects, layerDepth, timer.ToString(), spriteFont)
+        {
+            this.timer = timer;
+        }
+
+        public UITimer(Transform2D transform, float layerDepth, SpriteFont spriteFont, TimerUtility timer) :
+            base("Default Timer", ActorType.UIDynamicText, StatusType.Drawn | StatusType.Update, 
+                transform, Color.Red, SpriteEffects.None, layerDepth, 
+                timer.ToString(), spriteFont)
+        {
+            this.timer = timer;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Text = timer.ToString();
+            base.Update(gameTime);
+        }
+    }
+}
