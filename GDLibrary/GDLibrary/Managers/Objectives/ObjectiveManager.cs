4033e18757394b44a7dbf2f843aac6f052a3cb14
Unknown <d00212721@student.dkit.ie>
2019-11-14 21:30:37 +0000

    created objective manager

---
+++ GDLibrary/GDLibrary/Managers/Objectives/ObjectiveManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace GDLibrary
+{
+    public class ObjectiveManager : PausableDrawableGameComponent
+    {
+        #region Fields
+        private Game game;
+        private List<Actor2D> drawList, removeList;
+        private SpriteBatch spriteBatch;
+
+        public ObjectiveManager(Game game, EventDispatcher eventDispatcher, StatusType  statusType,int initialSize,SpriteBatch spriteBatch)
+            : base(game, eventDispatcher, statusType)
+        {
+            this.drawList  = new List<Actor2D>(initialSize);
+            this.removeList = new List<Actor2D>(initialSize);
+            this.spriteBatch = spriteBatch;
+        }
+
+
+
+
+
+
+
+        #endregion
+
+
+
+    }
+}
