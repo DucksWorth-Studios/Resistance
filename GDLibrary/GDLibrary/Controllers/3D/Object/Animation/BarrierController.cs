ead7687b9b9ecebb21b9ac828af9787f9a5e079e
TheLazyHatGuy <1010359+TheLazyHatGuy@users.noreply.github.com>
2019-11-18 22:22:20 +0000

    Add a basic barrier controller
    This will be used to program the animation

---
+++ GDLibrary/GDLibrary/Controllers/3D/Object/Animation/BarrierController.cs
@@ -0,0 +1,37 @@
+﻿using JigLibX.Math;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace GDLibrary
+{
+    public class BarrierController : Controller
+    {
+        private bool rotateClockwise;
+
+        public BarrierController(bool rotateClockwise, string id, ControllerType controllerType) : base(id, controllerType)
+        {
+            this.rotateClockwise = rotateClockwise;
+        }
+
+        #region Event Handeling
+
+        protected override void RegisterForEventHandling(EventDispatcher eventDispatcher)
+        {
+            base.RegisterForEventHandling(eventDispatcher);
+        }
+
+        #endregion
+
+        public override void Update(GameTime gameTime, IActor actor)
+        {
+            Actor3D parent = actor as Actor3D;
+
+            if (rotateClockwise)
+                parent.Transform.RotateAroundZBy(1);
+            else
+                parent.Transform.RotateAroundZBy(-1);
+
+            base.Update(gameTime, actor);
+        }
+    }
+}
