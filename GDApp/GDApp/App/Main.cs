1dccc1e3c37386f2c789b52f069d0dcfdd428e4c
Tomas <timkanders@gmail.com>
2019-12-04 19:16:06 +0000

    Added Exit Hall End Wall

--- GDApp/GDApp/App/Main.cs
+++ GDApp/GDApp/App/Main.cs
@@ -901,6 +901,31 @@ namespace GDApp
             this.objectManager.Add(clonePlane);
             #endregion
 
+
+            #region Exit End Wall
+            clonePlane = (CollidableObject)prototypeModel.Clone();
+            clonePlane.EffectParameters.Texture = this.textureDictionary["wall"];
+            clonePlane.Transform.Scale = new Vector3(xScale, 1, worldScale / 10.0f);
+            clonePlane.Transform.Translation = new Vector3((-2.493f * worldScale) / 2.2f, (2.54f * worldScale) / 20.0f, (6.3f * worldScale) / 2f);
+            clonePlane.AddPrimitive(new Box(clonePlane.Transform.Translation, Matrix.CreateRotationX(MathHelper.PiOver2),
+                new Vector3(clonePlane.Transform.Scale.X * 2.54f, clonePlane.Transform.Scale.Y * 2.54f, clonePlane.Transform.Scale.Z * 2.54f)),
+                new MaterialProperties(0.1f, 0.1f, 0.1f));
+            clonePlane.Enable(true, 1);
+            this.objectManager.Add(clonePlane);
+
+            //right side of door
+            clonePlane = (CollidableObject)prototypeModel.Clone();
+            clonePlane.EffectParameters.Texture = this.textureDictionary["wall"];
+            clonePlane.Transform.Scale = new Vector3(xScale *0.8f, 1, worldScale / 10.0f);
+            clonePlane.Transform.Translation = new Vector3((-2.55f * worldScale) / 3.5f, (2.54f * worldScale) / 20.0f, (6.3f * worldScale) / 2f);
+            clonePlane.AddPrimitive(new Box(clonePlane.Transform.Translation, Matrix.CreateRotationX(MathHelper.PiOver2),
+                new Vector3(clonePlane.Transform.Scale.X * 2.54f, clonePlane.Transform.Scale.Y * 2.54f, clonePlane.Transform.Scale.Z * 2.54f)),
+                new MaterialProperties(0.1f, 0.1f, 0.1f));
+            clonePlane.Enable(true, 1);
+            this.objectManager.Add(clonePlane);
+            #endregion
+
+
             #region dividing wall
             //right side
             clonePlane = (CollidableObject)prototypeModel.Clone();
